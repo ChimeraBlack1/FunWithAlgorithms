91738cddcb2c604ecb1db0d3b9342098c8754e38
chimerablack1 <christopherparke@zoho.com>
2019-09-03 20:00:14 -0400

    initial commit - binary algorithm

---
+++ binarySearch.py
@@ -0,0 +1,19 @@
+import math
+
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+xmin = 0
+xmax = len(primes) - 1
+target = 67
+iteration = 0
+
+for x in primes:
+    iteration = iteration + 1
+    arrVal = math.floor((xmin + xmax) / 2)
+    if primes[arrVal] < target:
+        xmin = arrVal + 1
+    elif primes[arrVal] > target:
+        xmax = arrVal - 1
+    else:
+        print("I found it in " + str(iteration)+ " tries. It's value " + str(primes[arrVal]) + " at index " + str(arrVal))
+        break
